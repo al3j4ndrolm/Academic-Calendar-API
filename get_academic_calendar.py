7cb4d9e2e75862efc6bdf9fa12556a01f04c8069
Al3j4ndrolm <lopezalejandro5b@gmail.com>
2025-04-23 13:11:02 -0700

    [Fix] Set up the right API port

--- get_academic_calendar.py
+++ get_academic_calendar.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 from bs4 import BeautifulSoup
+import os
 import requests
 
 app = Flask(__name__)
@@ -31,4 +32,6 @@ def get_calendar():
     return jsonify(calendar)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
